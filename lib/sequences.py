708db093ea116b388b398869528384ae99eb4a76
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-05-09 13:14:36 +0300

    createed sequence to pass lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    """ 
+    Print the Fibonacci sequence up to a given length."""
+    if length <= 0:
+        print([])
+        return
+
+    fib_sequence = [0, 1]
+    for i in range(2, length):
+        next_fib = fib_sequence[i-1] + fib_sequence[i-2]
+        fib_sequence.append(next_fib)
+    print(fib_sequence[:length])
+
+
+   
+    
+
